63548de0970d9a198fa330766c2c0ef317d2f9e6
Name <email@example.ru>
2025-04-09 09:59:25 +0300

    Добавьте файлы проекта.

---
+++ M35SocialNetwork/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace M35SocialNetwork.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
